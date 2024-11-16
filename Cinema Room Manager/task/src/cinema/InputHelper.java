5e67dd1310809d4586f91959c4b9622bf1a62d4b
Álvaro Zájara <alvaro.zajara@eviden.com>
2024-11-16 11:58:02 +0100

    Added InputHelper class

---
+++ Cinema Room Manager/task/src/cinema/InputHelper.java
@@ -0,0 +1,21 @@
+package cinema;
+
+import java.util.Scanner;
+
+public class InputHelper {
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public int getInt(String message){
+        System.out.println(message);
+        return scanner.nextInt();
+    }
+
+    public String getString(String message){
+        System.out.println(message);
+        return scanner.nextLine();
+    }
+
+    public static void closeScanner(){
+        scanner.close();
+    }
+}
